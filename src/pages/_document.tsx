37d4fba07d9b608613948b2a8a2e685007727ab9
EvelinSu <eveelinsu@gmail.com>
2023-10-06 15:16:50 +0500

    fix: fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,10 @@
 import Document, {
     DocumentContext,
-    DocumentInitialProps
+    DocumentInitialProps,
+    Html,
+    Main,
+    Head,
+  NextScript
 } from 'next/document';
 import { ServerStyleSheet } from "styled-components";
 
@@ -31,5 +35,23 @@ export default class MyDocument extends Document {
         } finally {
             sheet.seal()
         }
+    }
+    render() {
+        return (
+          <Html>
+              <Head>
+                  <link rel="preconnect" href="https://fonts.googleapis.com" />
+                  <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+                  <link
+                    href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600&display=swap"
+                    rel="stylesheet"
+                  />
+              </Head>
+              <body>
+              <Main />
+              <NextScript />
+              </body>
+          </Html>
+        )
     }
 }
